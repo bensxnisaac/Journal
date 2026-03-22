b5f77c5e3c5dda6d97355d66d663aeb76d30b522
bensxnisaac <bensxnisaac@gmail.com>
2026-03-22 10:34:03 +0100

    fix: React import in Trades.tsx

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,7 @@ app.use(cors({
   credentials: true,
 }));
 app.use(express.json({ limit: '2mb' }));
-app.use(cookieParser());
+app.use(cookieParser() as express.RequestHandler);
 
 app.use('/api/auth',   authRoutes);
 app.use('/api/trades', tradeRoutes);
